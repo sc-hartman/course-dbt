513b933348a092d9e40d5fab5e841eb7e0d787d7
Cory Hartman <seth.c.hartman@gmail.com>
2022-10-05 17:58:46 +0000

    update to orders.sql

--- greenery/models/staging/greenery/_src_orders.sql
+++
@@ -1,7 +0,0 @@
--- CTE to denote which source the staging table is being built from
-WITH orders AS(
-    SELECT * FROM {{ source('src_greenery', 'orders') }}
-)
-
-SELECT *
-FROM orders
---
+++ greenery/models/staging/greenery/src_orders.sql
@@ -0,0 +1,19 @@
+-- CTE to denote which source the staging table is being built from
+WITH orders AS(
+    SELECT * FROM {{ source('src_greenery', 'orders') }}
+)
+
+SELECT ORDER_ID AS ORDER_GUID
+       ,USER_ID AS USER_GUID
+       ,PROMO_ID AS PROMOTION_TYPE
+       ,ADDRESS_ID AS ADDRESS_GUID
+       ,CREATED_AT
+       ,ORDER_COST
+       ,SHIPPING_COST
+       ,ORDER_TOTAL
+       ,TRACKING_ID AS TRACKING_GUID
+       ,SHIPPING_SERVICE
+       ,ESTIMATED_DELIVERY_AT
+       ,DELIVERED_AT
+       ,STATUS AS ORDER_STATUS
+FROM orders
