32166b1a0bcebb133871803bb21fd78cba6eacf5
Cory Hartman <seth.c.hartman@gmail.com>
2022-10-06 15:35:52 +0000

    snapshot logic update

--- greenery/snapshots/snapshot_orders.sql
+++ greenery/snapshots/snapshot_orders.sql
@@ -2,8 +2,8 @@
 
 {{
     config(
-        target_database = 'DEV_DB',
-        target_schema = 'DBT_CORY',
+        target_database = target.database,
+        target_schema = target.schema,
         strategy='check',
         unique_key='order_id',
         check_cols=['status']
